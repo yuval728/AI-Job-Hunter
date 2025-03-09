e70432b3840f9a95e9aa1a5f5f0c228459ce1a47
Yuval728 <yuvalmehta.728@gmail.com>
2025-03-09 16:40:44 +0530

    Refactor Ai class structure, update agent configurations, and enhance task definitions in crew.py; adjust dependency versions in pyproject.toml

--- ai/src/ai/crew.py
+++ ai/src/ai/crew.py
@@ -1,136 +1,169 @@
 from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool, ScrapeWebsiteTool, FileReadTool, PDFSearchTool
+import warnings
+# import configparser
+warnings.filterwarnings("ignore")
+# config = configparser.ConfigParser(interpolation=None)
+
+llm = LLM(
+	# model="groq/llama-3.3-70b-specdec",
+    model='groq/deepseek-r1-distill-llama-70b',
+	# model='groq/gemma2-9b-it',
+	temperature=0.2,
+)
+
+manager_llm = LLM(
+	model="groq/qwen-2.5-32b",
+	temperature=0.2,
+)
 
 
 @CrewBase
-class Ai():
-	"""Ai crew"""
+class Ai:
+    """Ai crew"""
 
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
 
 
-	llm = LLM(
-		model='groq/llama-3.3-70b-versatile',
-		temperature=0.2,
-	)
-	
-	manager_llm = LLM(
-		model='groq/qwen-2.5-32b',
-		temperature=0.2,
-	)
- 
-	def set_tools(self, file_path):
-		self.search_tool = SerperDevTool()
-		self.scrape_tool = ScrapeWebsiteTool()
-		self.read_resume = FileReadTool(file_path)
-		self.semantic_search_job = PDFSearchTool(
-			pdf=file_path,
-			config=dict(
-				llm=dict(
-					provider="groq",
-					config=dict(
-						model="mixtral-8x7b-32768",
-						# model="groq/llama-3.3-70b-versatile",
-					),
-				),
-				embedder=dict(
-					provider="huggingface",
-					config=dict(
-						model="BAAI/bge-large-en-v1.5",
-						# task_type="retrieval_document",
-					),
-				),
-			),
-		)
-    
-	
+    function_calling_llm = LLM(
+        model="groq/llama-3.3-70b-specdec",
+        temperature=0.2,
+    )
+    def __init__(self, file_path):
+        self.search_tool = SerperDevTool()
+        self.scrape_tool = ScrapeWebsiteTool()
+        # self.read_resume = FileReadTool(file_path)
+        self.semantic_search_job = PDFSearchTool(
+            pdf=file_path,
+            config=dict(
+                llm=dict(
+                    provider="groq",
+                    config=dict(
+                        model="mixtral-8x7b-32768",
+                        # model="groq/llama-3.3-70b-versatile",
+                    ),
+                ),
+                embedder=dict(
+                    provider="huggingface",
+                    config=dict(
+                        model="BAAI/bge-large-en-v1.5",
+                        # task_type="retrieval_document",
+                    ),
+                ),
+            ),
+        )
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			verbose=True,
-			llm=self.llm,
-			tools=[self.search_tool, self.scrape_tool],
-		)
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["researcher"],
+            verbose=True,
+            llm=llm,
+            tools=[self.search_tool, self.scrape_tool],
+            # function_calling_llm=self.function_calling_llm,
+            # max_retry_limit=100,
+        )
 
-	@agent
-	def profiler(self) -> Agent:
-		return Agent(
-			config=self.agents_config['profiler'],
-			verbose=True,
-			llm=self.llm,
-			tools=[self.search_tool, self.scrape_tool, self.read_resume, self.semantic_search_job], 
-		)
+    @agent
+    def profiler(self) -> Agent:
+        return Agent(
+            config=self.agents_config["profiler"],
+            verbose=True,
+            llm=llm,
+            tools=[
+                # self.search_tool,
+                self.scrape_tool,
+                # self.read_resume,
+                self.semantic_search_job,
+            ],
+            # function_calling_llm=self.function_calling_llm,
+            max_rpm=3,
+			# max_retry_limit=100,
+        )
 
-	@agent
-	def resume_strategist(self) -> Agent:
-		return Agent(
-			config=self.agents_config['resume_strategist'],
-			verbose=True,
-			llm=self.llm,
-			tools=[self.search_tool, self.scrape_tool, self.read_resume, self.semantic_search_job],
-		)
-  
-	@agent
-	def interview_preparer(self) -> Agent:
-		return Agent(
-			config=self.agents_config['interview_preparer'],
-			verbose=True,
-			llm=self.llm,
-			tools=[self.search_tool, self.scrape_tool, self.read_resume, self.semantic_search_job],
-		)
-  
+    @agent
+    def resume_strategist(self) -> Agent:
+        return Agent(
+            config=self.agents_config["resume_strategist"],
+            verbose=True,
+            llm=llm,
+            tools=[
+                # self.read_resume,
+                self.search_tool,
+                self.scrape_tool,
+                self.semantic_search_job,
+            ],
+            # function_calling_llm=self.function_calling_llm,
+            max_rpm=3,
+            # max_retry_limit=100,
+        )
 
-	@task
-	def research_task(self) -> Task:
-		self.research_task_instance = Task(
-			config=self.tasks_config['research_task'],
-			agent=self.researcher,
-			# async_execution=True,
-		)
-		return self.research_task_instance
+    @agent
+    def interview_preparer(self) -> Agent:
+        return Agent(
+            config=self.agents_config["interview_preparer"],
+            verbose=True,
+            llm=llm,
+            tools=[
+                self.search_tool,
+                self.scrape_tool,
+                # self.read_resume,
+                self.semantic_search_job,
+            ],
+            # function_calling_llm=self.function_calling_llm,
+            max_rpm=3,
+            # max_retry_limit=100,
+        )
 
-	@task
-	def profile_task(self) -> Task:
-		self.profile_task_instance = Task(
-			config=self.tasks_config['profile_task'],
-			agent=self.profiler,
-			# async_execution=True,
-		)
-		return self.profile_task_instance
-  
-	@task
-	def resume_strategy_task(self) -> Task:
-		self.resume_strategy_task_instance = Task(
-			config=self.tasks_config['resume_strategy_task'],
-			agent=self.resume_strategist,
-			context=[self.profile_task_instance, self.research_task_instance],
-			# async_execution=True,
-		)
-		return self.resume_strategy_task_instance
+    @task
+    def research_task(self) -> Task:
+        self.research_task_instance = Task(
+            config=self.tasks_config["research_task"],
+            # async_execution=True,
+        )
+        return self.research_task_instance
 
-	@task
-	def interview_preparation_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['interview_preparation_task'],
-			agent=self.interview_preparer,
-			context=[self.profile_task_instance, self.resume_strategy_task_instance, self.research_task_instance],
-			# async_execution=True,
-		)
+    @task
+    def profile_task(self) -> Task:
+        self.profile_task_instance = Task(
+            config=self.tasks_config["profile_task"],
+            # async_execution=True,
+        )
+        return self.profile_task_instance
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Ai crew"""
-	
+    @task
+    def resume_strategy_task(self) -> Task:
+        self.resume_strategy_task_instance = Task(
+            config=self.tasks_config["resume_strategy_task"],
+            context=[self.profile_task_instance, self.research_task_instance],
+            # async_execution=True,
+        )
+        return self.resume_strategy_task_instance
 
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			# process=Process.sequential,
-			verbose=True,
-			manager_llm=self.manager_llm
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @task
+    def interview_preparation_task(self) -> Task:
+        self.interview_preparation_task_instance = Task(
+            config=self.tasks_config["interview_preparation_task"],
+            context=[
+                self.resume_strategy_task_instance,
+                self.profile_task_instance,
+                self.research_task_instance,
+            ],
+            # async_execution=True,
+        )
+        return self.interview_preparation_task_instance
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Ai crew"""
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            manager_llm=manager_llm,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
